165b92e27055e9d7f746dc58db0580f3c39b7011
Ramya.U <ramyayadav2016@gmail.com>
2022-12-13 20:53:15 +0530

    creation of the project

---
+++ Tufillo-MVC-and-EFCore/Tufillo - MVC and EFCore/Tufillo - MVC and EFCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tufillo___MVC_and_EFCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
