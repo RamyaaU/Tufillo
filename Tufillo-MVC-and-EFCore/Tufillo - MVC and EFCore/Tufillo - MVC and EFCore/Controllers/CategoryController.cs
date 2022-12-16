6e98616eb42477509c5046a09ab7fbf282e3a059
Ramya.U <ramyayadav2016@gmail.com>
2022-12-16 22:13:50 +0530

    ui changes for category view

--- Tufillo-MVC-and-EFCore/Tufillo - MVC and EFCore/Tufillo - MVC and EFCore/Controllers/CategoryController.cs
+++ Tufillo-MVC-and-EFCore/Tufillo - MVC and EFCore/Tufillo - MVC and EFCore/Controllers/CategoryController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using Tufillo.Infrastructure.Data;
+using Tufillo___MVC_and_EFCore.Models;
 
 namespace Tufillo___MVC_and_EFCore.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly TufilloContext _dbContext;
+
+        public CategoryController(TufilloContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objects = _dbContext.Category;
+            return View(objects);
         }
     }
 }
