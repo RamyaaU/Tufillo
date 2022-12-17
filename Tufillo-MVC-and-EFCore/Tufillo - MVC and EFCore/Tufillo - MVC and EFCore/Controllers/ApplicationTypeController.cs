da0197f6c0390521182382cdc15f57d2d5bc55d6
Ramya.U <ramyayadav2016@gmail.com>
2022-12-17 16:07:33 +0530

    added controller for application type

---
+++ Tufillo-MVC-and-EFCore/Tufillo - MVC and EFCore/Tufillo - MVC and EFCore/Controllers/ApplicationTypeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Tufillo.Infrastructure.Data;
+using Tufillo.Infrastructure.Models;
+
+namespace Tufillo___MVC_and_EFCore.Controllers
+{
+    public class ApplicationTypeController : Controller
+    {
+        private readonly TufilloContext _dbContext;
+
+        public ApplicationTypeController(TufilloContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationType> objects = _dbContext.ApplicationType;
+            return View(objects);
+        }
+
+        //GET - Create 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ApplicationType applicationType)
+        {
+            _dbContext.ApplicationType.Add(applicationType);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
