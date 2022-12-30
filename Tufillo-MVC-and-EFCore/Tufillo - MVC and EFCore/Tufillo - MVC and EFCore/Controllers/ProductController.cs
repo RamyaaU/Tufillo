8fdcd28876c182b90c4500da5af8863a4a9c3b41
Ramya.U <ramyayadav2016@gmail.com>
2022-12-30 17:11:26 +0530

    added product controller method

---
+++ Tufillo-MVC-and-EFCore/Tufillo - MVC and EFCore/Tufillo - MVC and EFCore/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Tufillo.Infrastructure.Data;
+using Tufillo.Infrastructure.Models;
+
+namespace Tufillo___MVC_and_EFCore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly TufilloContext _dbContext;
+
+        public ProductController(TufilloContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> product = _dbContext.Product;
+
+            foreach ( var obj in product )
+            {
+                obj.Category = _dbContext.Category.FirstOrDefault(x => x.Id == obj.CategoryId);
+            }
+
+            return View(product);
+        }
+    }
+}
