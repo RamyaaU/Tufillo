79014a29b4dcc441b7f9551fadc6db83716b9921
Ramya.U <ramyayadav2016@gmail.com>
2022-12-30 16:35:33 +0530

    added product model class

---
+++ Tufillo-MVC-and-EFCore/Tufillo - MVC and EFCore/Tufillo.Infrastructure/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Tufillo.Infrastructure.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }    
+
+        public string Description { get; set; }
+
+        [Range(1,int.MaxValue)]
+        public double Price { get; set; }
+
+        public string Image { get; set; }
+
+        //to configure FK relation to CategoryId, create a property 
+        //by this EF automatically adds a mapping between category and product
+        //for EF to make it aware regarding the FK relationship, dataannotation must be added
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+
+        //FK data column 
+        [Display(Name="Category Type")]
+        public int CategoryId { get; set; }
+    }
+}
