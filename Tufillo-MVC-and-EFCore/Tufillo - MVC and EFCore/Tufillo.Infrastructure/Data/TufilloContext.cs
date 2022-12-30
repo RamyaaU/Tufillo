5c47c177103db13a93bd9d4c5b4b9a658912b1cb
Ramya.U <ramyayadav2016@gmail.com>
2022-12-30 16:46:38 +0530

    adding migration and pushing into db

--- Tufillo-MVC-and-EFCore/Tufillo - MVC and EFCore/Tufillo.Infrastructure/Data/TufilloContext.cs
+++ Tufillo-MVC-and-EFCore/Tufillo - MVC and EFCore/Tufillo.Infrastructure/Data/TufilloContext.cs
@@ -17,5 +17,6 @@ namespace Tufillo.Infrastructure.Data
         public DbSet<Category> Category { get; set; }
 
         public DbSet<ApplicationType> ApplicationType { get; set; }
+        public DbSet<Product> Product { get; set; }
     }
 }
